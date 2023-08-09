acff31bcdc205b6fe881ae46644475c64981b8ef
Sumit Paul <pauls1317@gmail.com>
2023-08-10 00:12:09 +0530

    updated color generator

--- 09-color-generator/setup/README.md
+++ 09-color-generator/setup/README.md
@@ -1,3 +1,14 @@
-#### Values JS
+### Overview
+Color shades and tints generator with the help of values.js
+
+## Concepts used
+1. Array join() method
+2. Array spread operator
+3. toString() method to get hex value
+
+## Values JS
 
 [values.js](https://github.com/noeldelgado/values.js)
+
+## Final UI
+.[](./screenshot.png)
--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values('#f15025').all(10));
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
@@ -17,24 +16,45 @@ function App() {
     } catch (error) {
       setError(true);
     }
-    console.log('Hello');
-  }
+    console.log("Hello");
+  };
 
-  return <>
-    <section className="container">
-      <h3>color generator</h3>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={color} onChange={(e)=>{setColor(e.target.value);}} placeholder='#f15025' className={error?'error':''}/>
-        <button className='btn' type='submit'>Submit</button>
-      </form>
-    </section>
-    <section className='colors'>
-      {list.map((color, i)=>{
-        console.log(color);
-        return <SingleColor key={i} {...color} index={i} />
-      })}
-    </section>
-  </>
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+            placeholder="#f15025"
+            className={error ? "error" : ""}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <div className="info-container">
+          <h5>Get tints and shades of a CSS color</h5>
+          <p>
+            The lightness or darkness of a color is called its value. Tints are
+            light values that are made by mixing a color with white, which
+            increases lightness. Shades are dark values that are made by mixing
+            a color with black, which reduces lightness.
+          </p>
+        </div>
+      <section className="colors">
+        {list.map((color, i) => {
+          console.log(color);
+          return <SingleColor key={i} {...color} index={i} />;
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
